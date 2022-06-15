140ebf6740ac9a8920a3281eb3f02c01a7748bbf
Maximilian Fischer <maximilian.fischer@ifd-gmbh.com>
2022-06-15 11:23:57 +0200

    Renamed Main files to PalmEngineRoot

---
+++ PalmEngine/PalmEngineRoot.cpp
@@ -0,0 +1,14 @@
+#include "PalmEngineRoot.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl;
+
+	// Startup all Singletons
+
+	// Shutdown all Singletons
+
+	return 0;
+}
