3e0777aa1a894ec712dd87022f44e70716479eda
Maximilian Fischer <maximilian.fischer@ifd-gmbh.com>
2022-06-16 08:20:00 +0200

    New project structue and renamed main files to PalmEngine

---
+++ src/PalmEngine.cpp
@@ -0,0 +1,16 @@
+#include "PalmEngine.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl;
+
+	// Startup all Singletons
+
+	// Start the game loop
+
+	// Shutdown all Singletons
+
+	return 0;
+}
