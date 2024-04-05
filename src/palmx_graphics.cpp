e0f74a11a6c3dcc66d780ed204d494ef97dc95b3
getmyisland <getmyisland@gmail.com>
2024-04-05 17:29:57 +0200

    feat: upgrade assimp

--- src/palmx_graphics.cpp
+++ src/palmx_graphics.cpp
@@ -580,8 +580,29 @@ namespace palmx
 	{
 		PALMX_ASSERT(px_data.init, "palmx not initialized");
 
+		unsigned int flags =
+			aiProcess_CalcTangentSpace | // calculate tangents and bitangents if possible
+			aiProcess_JoinIdenticalVertices | // join identical vertices/ optimize indexing
+			//aiProcess_ValidateDataStructure  | // perform a full validation of the loader's output
+			aiProcess_Triangulate | // Ensure all verticies are triangulated (each 3 vertices are triangle)
+			//aiProcess_ConvertToLeftHanded | // convert everything to D3D left handed space (by default right-handed, for OpenGL)
+			aiProcess_SortByPType | // ?
+			aiProcess_ImproveCacheLocality | // improve the cache locality of the output vertices
+			aiProcess_RemoveRedundantMaterials | // remove redundant materials
+			aiProcess_FindDegenerates | // remove degenerated polygons from the import
+			aiProcess_FindInvalidData | // detect invalid model data, such as invalid normal vectors
+			aiProcess_GenUVCoords | // convert spherical, cylindrical, box and planar mapping to proper UVs
+			aiProcess_TransformUVCoords | // preprocess UV transformations (scaling, translation ...)
+			aiProcess_FindInstances | // search for instanced meshes and remove them by references to one master
+			aiProcess_LimitBoneWeights | // limit bone weights to 4 per vertex
+			aiProcess_OptimizeMeshes | // join small meshes, if possible;
+			//aiProcess_PreTransformVertices | //-- fixes the transformation issue.
+			//aiProcess_SplitByBoneCount | // split meshes with too many bones. Necessary for our (limited) hardware skinning shader
+			aiProcess_FlipUVs | // flip all UVs or else textures will be messed up
+			0;
+
 		Assimp::Importer importer;
-		const aiScene* ai_scene = importer.ReadFile(file_path, aiProcess_Triangulate | aiProcess_FlipUVs);
+		const aiScene* ai_scene = importer.ReadFile(file_path, flags);
 
 		if (!ai_scene || ai_scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !ai_scene->mRootNode)
 		{
